7db4356bc032bbf8485498282bf57d815e45bc7a
flavian mohanraj <flavianmohanraj@gmail.com>
2020-12-03 19:54:46 -0500

    Using ngIf to Output Data Conditionally

--- udemy-angular/src/app/servers/servers.component.ts
+++ udemy-angular/src/app/servers/servers.component.ts
@@ -12,8 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created!'
-  serverName ="Test Server";
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,12 +24,12 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
   }
 
-  onUpdateServerName(event: Event){
+  onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
-
   }
 }
